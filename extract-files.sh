karin722/device_oneplus_guacamoleb

sm8150-common: Add QCOM's WFD implementation
* Sadly, out-of-the-box Miracast support was killed in P, so
  we need this to keep Wi-Fi display feature working.
* Additionally libwfdnative has been modified to load
  libhidlbase.so instead of android.hidl.base@1.0.so using
  following command:
    sed -i 's/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/' proprietary/lib64/libwfdnative.so proprietary/lib/libwfdnative.so

Change-Id: Id680d918b64c0e3733251db45992ec477b6fbdfb

extract-files.sh
@@ -71,4 +71,8 @@ extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
 extract "${MY_DIR}/proprietary-files-vendor.txt" "${SRC_VENDOR}" \
         "${KANG}" --section "${SECTION}"
 
+COMMON_BLOB_ROOT="${LINEAGE_ROOT}/vendor/${VENDOR}/${DEVICE_COMMON}/proprietary"
+
+sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${COMMON_BLOB_ROOT}/lib64/libwfdnative.so" "${COMMON_BLOB_ROOT}/lib/libwfdnative.so"
+
 "${MY_DIR}/setup-makefiles.sh"

Ashwin4RC/op7

extract-files.sh
@@ -64,4 +64,9 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEAN_VENDOR}"
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
         "${KANG}" --section "${SECTION}"
 
+
+COMMON_BLOB_ROOT="${LINEAGE_ROOT}/vendor/${VENDOR}/${DEVICE}/proprietary"
+
+sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${COMMON_BLOB_ROOT}/lib64/libwfdnative.so" "${COMMON_BLOB_ROOT}/lib/libwfdnative.so"
+
 "${MY_DIR}/setup-makefiles.sh"